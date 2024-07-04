515b7883eea5e659c4191ad9a372e9f3954402c2
Yohanes Harke Wauran <harkewauran23@gmail.com>
2024-07-04 14:46:22 +0800

    first commit

---
+++ lib/data.ts
@@ -0,0 +1,100 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import Project from "../public/project.jpg";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+  {
+    name: "Contact",
+    hash: "#contact",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Studi Independen Bersertifikat",
+    location: "Online",
+    description:
+      "I learned how to code and how to build websites using HTML, CSS, and JavaScript.",
+    icon: React.createElement(LuGraduationCap),
+    date: "February 2022 - August 2022",
+  },
+  {
+    title: "Machine Learning Developer",
+    location: "Online",
+    description:
+      "I learned how to code and how to build model machine learning using Python.",
+    icon: React.createElement(LuGraduationCap),
+    date: "October 2022 - December 2022",
+  },
+  {
+    title: "Movile Developer",
+    location: "Online",
+    description:
+      "I learned how to code and how to build mobile apps using Kotlin.",
+    icon: React.createElement(LuGraduationCap),
+    date: "February 2023 August 2023",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: Project,
+  },
+  {
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: Project,
+  },
+  {
+    title: "Lorem Ipsum",
+    description:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: Project,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "Redux",
+  "Express",
+  "PostgreSQL",
+  "Python",
+] as const;
